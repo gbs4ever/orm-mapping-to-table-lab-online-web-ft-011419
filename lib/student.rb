bb40c25261f1f6fc2250f311157aabe1cb27317c
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-15 18:44:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,15 @@ attr_reader :id
     @grade= grade
     @id=id
   end
+
+  def self.create_table
+    sql = <<- SQL
+      CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+      )
+      SQl
+  DB[:conn].execute(sql)
+  end
 end
