dc87e2ebce81173a9b44da6cb7ad5c11b2667211
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-15 18:56:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,7 +30,7 @@ sql = <<-SQL
 INSERT INTO students (name,grade)VALUES (?,?)
 SQL
 DB[:conn].execute(sql,name,grade)
-
+  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
 end
 def self.create
 
